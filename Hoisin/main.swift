c0eef5c2d18b38968a9df910314bb1197b0e7e74
Sidney San Martín <sidney@s4y.us>
2014-08-19 07:42:23 -0400

    Initial Commit

---
+++ Hoisin/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Hoisin
+//
+//  Created by Sidney San Martín on 8/19/14.
+//  Copyright (c) 2014 s4y. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
