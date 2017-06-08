e43dba253ce37e6223ccb1972035e2b0d3fe8bf8
Sidney San Martín <sidney@s4y.us>
2017-06-07 21:57:51 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -3,7 +3,7 @@
 char tty[24][80] = {{0}};
 
 int main() {
-	auto pp = [NSApplication sharedApplication];
+	auto app = [NSApplication sharedApplication];
 	auto win = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 100, 100) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO];
 	[win center];
 	[win makeKeyAndOrderFront:nil];
