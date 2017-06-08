558ae1d3dc95aaf684ff929a4c581f7670271a74
Sidney San Martín <sidney@s4y.us>
2017-06-07 22:02:22 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -5,6 +5,7 @@ char tty[24][80] = {{0}};
 int main() {
 	auto app = [NSApplication sharedApplication];
 	auto win = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 100, 100) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:NO];
+	win.contentView.wantsLayer = YES;
 	[win center];
 	[win makeKeyAndOrderFront:nil];
 
