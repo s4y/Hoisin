919fcc4dc7edb06f8eeb3263f02a2c6d1428b120
Sidney San Martín <sidney@s4y.us>
2017-06-07 22:04:02 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -1,5 +1,14 @@
 #import <Cocoa/Cocoa.h>
 
+@interface AppDelegate: NSObject<NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+	return YES;
+}
+
 char tty[24][80] = {{0}};
 
 int main() {
