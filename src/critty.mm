588d06d96624c2e44825ebebac208cab592725db
Sidney San Martín <sidney@s4y.us>
2017-06-08 09:05:25 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -9,7 +9,7 @@
 @implementation TerminalContentView
 - (void)drawRect:(NSRect)dirtyRect {
 	NSFont* systemFont = [NSFont systemFontOfSize:[NSFont systemFontSize]];
-	const CGFloat lineHeight = systemFont.xHeight;
+	const CGFloat lineHeight = systemFont.capHeight;
 	NSAttributedString* string = [[NSAttributedString alloc] initWithString:@"1234567890abcABCéø" attributes:@{
 		NSFontAttributeName: systemFont
 	}];
