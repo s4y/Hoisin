c3c5ea0248b15f33e456f026c3365863460abcfe
Sidney San Martín <sidney@s4y.us>
2017-06-11 13:49:47 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -75,7 +75,7 @@ const CGFloat systemFontHeight = NSHeight(systemFont.boundingRectForFont);
 	case NSStreamEventHasBytesAvailable: {
 		uint8_t buf[8192];
 		NSUInteger len = [_randle read:buf maxLength:sizeof(buf)/sizeof(buf[0])];
-		NSData* data = [NSData dataWithBytesNoCopy:buf length:len];
+		NSData* data = [NSData dataWithBytesNoCopy:buf length:len freeWhenDone:NO];
 		NSLog(@"read: %@", data);
 		[_contentView.textStorage appendAttributedString:[[NSAttributedString alloc] initWithString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]]];
 		[_contentView scrollPoint:NSMakePoint(0, NSMaxY(_contentView.bounds))];
