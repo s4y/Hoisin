3ffa241b93fb508ca980fe6cdc324066bd1dd008
Sidney San Martín <sidney@s4y.us>
2017-06-11 13:47:18 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -75,7 +75,10 @@ const CGFloat systemFontHeight = NSHeight(systemFont.boundingRectForFont);
 	case NSStreamEventHasBytesAvailable: {
 		uint8_t* buf;
 		NSUInteger len;
-		if (![_randle getBuffer:&buf length:&len]) { abort(); }
+		if (![_randle getBuffer:&buf length:&len]) {
+			NSLog(@"Couldn't get a buffer.");
+			abort();
+		}
 		NSData* data = [NSData dataWithBytesNoCopy:buf length:len];
 		NSLog(@"read: %@", data);
 		[_contentView.textStorage appendAttributedString:[[NSAttributedString alloc] initWithString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]]];
