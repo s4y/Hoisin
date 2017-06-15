0e66668bb07ffa0062a0925f36fa94c7f8eef65d
Sidney San Martín <sidney@s4y.us>
2017-06-15 02:39:21 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -23,7 +23,7 @@ const CGFloat systemFontHeight = NSHeight(systemFont.boundingRectForFont);
 	CTLineRef line = CTLineCreateWithAttributedString(static_cast<CFAttributedStringRef>([[NSAttributedString alloc] initWithString:self.string attributes:@{
 		NSFontAttributeName: systemFont
 	}]));
-	CGContextSetTextPosition(context, 0, 0);
+	CGContextSetTextPosition(context, 0, 2);
 	CTLineDraw(line, context);
 }
 @end
