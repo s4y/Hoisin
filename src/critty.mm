d52fb2fc2e12f1e6fc1ff2d9f38f078b9087e02d
Sidney San Martín <sidney@s4y.us>
2017-07-22 03:07:21 -0400

    WIP

--- src/critty.mm
+++ src/critty.mm
@@ -42,7 +42,7 @@ const CGFloat systemFontHeight = NSHeight(systemFont.boundingRectForFont);
 
 - (CALayer*)makeBackingLayer {
 	CALayer* layer = [super makeBackingLayer];
-	layer.backgroundColor = NSColor.greenColor.CGColor;
+	//layer.backgroundColor = NSColor.greenColor.CGColor;
 	return layer;
 }
 
