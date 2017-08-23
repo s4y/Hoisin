ec587b2c40c0898147dcfd04708469e3920d0f8f
Sidney San Martín <sidney@s4y.us>
2017-08-22 21:29:06 -0400

    WIP

--- src/critty.m
+++ src/critty.m
@@ -1,4 +1,5 @@
 #include "utf8/utf8.h"
+#include "tinybuf/tinybuf.h"
 
 #include <math.h>
 #include <stdlib.h>
@@ -26,20 +27,52 @@
 	// Consider saving the original data.
 	// NSMutableArray<dispatch_data_t>* _originalData;
 	NSMutableArray<TerminalStorageLine*>* _lines;
+	size_t _currentLine;
+	tinybuf_t _buf;
 	utf8_decode_context_t _utf8_decode_context;
 }
 
 - (instancetype)init {
 	if ((self = [super init])) {
 		_lines = [NSMutableArray array];
+		_currentLine = -1;
 	}
 	return self;
 }
 
+- (void)dealloc {
+	tinybuf_free(&_buf);
+}
+
 - (void)append:(dispatch_data_t)data {
+	__block size_t good_length = 0;
 	dispatch_data_apply(data, ^bool(dispatch_data_t region, size_t offset, const void *buffer, size_t size) {
+		for (size_t i = 0; i < size; i++) {
+			utf8_decode(&_utf8_decode_context, ((unsigned char*)buffer)[i]);
+			switch (_utf8_decode_context.state) {
+				case UTF8_OK:
+					tinybuf_append(&_buf, _utf8_decode_context.codepoint);
+					good_length = _buf.len;
+					break;
+				case UTF8_ERROR:
+					/* Append a replacement character (�) */
+					tinybuf_append(&_buf, 0xfffd);
+					_utf8_decode_context.state = UTF8_OK;
+					break;
+				default:
+					break;
+			}
+		}
 		return true;
 	});
+	if (!good_length) {
+		// No whole codepoints, nothing to do.
+		return;
+	}
+	for (size_t i = 0; i < good_length; i++) {
+	}
+
+
 	[_observer terminalStorage:self changedLines:@[]];
 }
 @end
---
+++ src/tinybuf/tinybuf.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdlib.h>
+
+#define TINYBUF_GROW 1024
+
+typedef struct {
+	size_t cap;
+	size_t len;
+	uint32_t* buf;
+} tinybuf_t;
+
+static inline void tinybuf_init(tinybuf_t* tbuf) {
+	*tbuf = (tinybuf_t){0};
+}
+
+static inline void tinybuf_free(tinybuf_t* tbuf) {
+	if (!tbuf->buf)
+		return;
+	free(tbuf->buf);
+}
+
+static inline void tinybuf_append(tinybuf_t* tbuf, uint32_t val) {
+	if (tbuf->len == tbuf->cap) {
+		tbuf->cap += TINYBUF_GROW;
+		tbuf->buf = realloc(tbuf->buf, tbuf->cap);
+	}
+	tbuf->buf[tbuf->len++] = val;
+}
