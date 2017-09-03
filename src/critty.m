8124e816dac425579da31c8768e5c52fceb6c7d4
Sidney San Martín <sidney@s4y.us>
2017-09-02 20:15:44 -0400

    WIP

--- src/critty.m
+++ src/critty.m
@@ -70,7 +70,7 @@ static const CGFloat kLineXMargin = 4;
 }
 
 - (void)performWithLines:(void(^)(NSArray<TerminalDocumentLine*>*))block {
-	dispatch_async(_queue, ^{ block(_lines); });
+	dispatch_sync(_queue, ^{ block(_lines); });
 }
 
 #if 0
