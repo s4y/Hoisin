3c490031fff01fc580cb9dc44698348c7e5d8d44
Sidney San Martín <sidney@s4y.us>
2017-09-05 08:12:35 -0400

    WIP

--- src/critty.m
+++ src/critty.m
@@ -414,6 +414,8 @@ int main(int argc, char* argv[]) {
 	win.frameAutosaveName = @"Window";
 	[win makeKeyAndOrderFront:nil];
 
+	sleep(5);
+
 	TerminalDocument* document = [[TerminalDocument alloc] init];
 	terminalView.document = document;
 
