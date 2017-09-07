0bf298c3a43d9019910a52f686efbf6a7fe368f6
Sidney San Martín <sidney@s4y.us>
2017-09-07 12:05:25 -0400

    WIP

--- src/critty.m
+++ src/critty.m
@@ -211,8 +211,6 @@ size_t lineId = 0;
 	CTLineRef line = CTLineCreateWithAttributedString((CFAttributedStringRef)([[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%zu %zu %@", _index, _id, string ? string : @"<nil>"] attributes:@{
 		NSFontAttributeName: _font,
 	}]));
-	[NSColor.whiteColor setFill];
-	CGContextFillRect(context, dirtyRect);
 	CGContextSetTextPosition(context, 0, ceil(-_font.descender));
 	CTLineDraw(line, context);
 	CFRelease(line);
