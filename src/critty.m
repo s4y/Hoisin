880876efa88bd8090f87b3595ab8282e5af97db7
Sidney San Martín <sidney@s4y.us>
2017-09-07 11:15:29 -0400

    WIP

--- src/critty.m
+++ src/critty.m
@@ -435,6 +435,7 @@ int main(int argc, char* argv[]) {
 	dispatch_source_set_event_handler(timer, ^{
 		[terminalView.contentView prepareContentInRect:NSZeroRect];
 		terminalView.contentView.needsDisplay = YES;
+		[terminalView.contentView displayIfNeeded];
 	});
 	dispatch_resume(timer);
 
