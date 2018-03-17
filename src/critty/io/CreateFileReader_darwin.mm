32bbf742f21ecb916e1e4162b05d0e894c82d776
Sidney San Martín <sidney@s4y.us>
2018-03-17 01:12:11 -0400

    WIP

--- src/critty/io/CreateFileReader_darwin.mm
+++ src/critty/io/CreateFileReader_darwin.mm
@@ -6,12 +6,23 @@ namespace critty {
 namespace io {
 
 std::unique_ptr<Reader> CreateFileReader(const char* path) {
-	struct FileReaderReader: Reader {
+	struct FileReader final: Reader {
 		dispatch_queue_t queue;
 		dispatch_io_t channel;
+
+		FileReader(const char* path) :
+			channel{dispatch_io_create_with_path(
+				DISPATCH_IO_STREAM, path, O_RDONLY, 0, queue, ^(int err){}
+			)}
+		{}
+
+		void resume() {}
+		void pause() {}
+		void close() {}
+		
 	};
 
-	return nullptr;
+	return std::make_unique<FileReader>(path);
 }
 
 }  // namespace io
