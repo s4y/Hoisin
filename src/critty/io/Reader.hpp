6c3edc34059d7cc0a6283f27cc399d02c18c3b26
Sidney San Martín <sidney@s4y.us>
2018-03-27 18:42:04 -0400

    WIP

--- src/critty/io/Reader.hpp
+++ src/critty/io/Reader.hpp
@@ -10,12 +10,7 @@ struct Reader {
 		virtual ~Observer() {}
 		virtual void didRead(void* buf, size_t len) = 0;
 	};
-
-	std::unique_ptr<Observer> observer;
-
-	virtual void resume() = 0;
-	virtual void pause() = 0;
-	virtual void close() = 0;
+	virtual ~Reader() = 0;
 };
 
 }  // namespace io
