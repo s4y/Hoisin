c858697bb9ea80ae9b5d51c5d188872c6a1b5ba2
Sidney San Martín <sidney@s4y.us>
2018-08-26 00:15:09 -0400

    WIP

--- src/cocoa/Document.mm
+++ src/cocoa/Document.mm
@@ -5,6 +5,10 @@
 @end
 
 @implementation Document
++ (BOOL)autosavesInPlace {
+	return YES;
+}
+
 - (void)makeWindowControllers {
 	NSWindow* window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 300, 300) styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskResizable|NSWindowStyleMaskClosable backing:NSBackingStoreBuffered defer:NO];
 	[window cascadeTopLeftFromPoint:NSMakePoint(100, 100)];
